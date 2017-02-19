c8f582dcd9b8f11ae468d36b382ff1faebdc07e1
zhaobinforward <312847654@qq.com>
2017-02-19 22:48:31 +0800

    delete

--- index.php
+++ index.php
@@ -4,19 +4,19 @@ require 'config.php';
 require R_ROOT.'/data/awesome_content.php';
 require R_ROOT.'/include/inc.session.php';
 
-$showpic = 0;
-//手机作品展示*4
-$sql = "select pickey9 from skindesignjxh2017_upload where checked=1 and status=1 order by create_time desc limit 4";
-$wtop3 = $MDB->db_query($sql);
-foreach($wtop3 as $k=>&$v){
-	$v['pickey9'] = CLOUD_URL.end(explode("/",$v['pickey9']));
-}
-unset($v);
-
-
-if( count($wtop3)>=3 ){
-	$showpic = 1;
-}
+//$showpic = 0;
+////手机作品展示*4
+//$sql = "select pickey9 from skindesignjxh2017_upload where checked=1 and status=1 order by create_time desc limit 4";
+//$wtop3 = $MDB->db_query($sql);
+//foreach($wtop3 as $k=>&$v){
+//	$v['pickey9'] = CLOUD_URL.end(explode("/",$v['pickey9']));
+//}
+//unset($v);
+//
+//
+//if( count($wtop3)>=3 ){
+//	$showpic = 1;
+//}
 
 include template('index');
 exit;
